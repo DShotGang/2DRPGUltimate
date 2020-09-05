ca1b3a913761c0e6c0be5cfb95d95be900247051
DShotGang <dshotgang@outlook.com>
2020-09-05 18:41:43 -0300

    v0.065
    Beginning of dialogue and some attributes

---
+++ Assets/Scripts/GokuDialog.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GokuDialog : MonoBehaviour
+{
+
+    public GameObject Projectile;
+    public GameObject Explosion;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionStay2D(Collision2D collider)
+    {
+        if (collider.transform.tag == "Projectile")
+        {
+
+        }
+    }
+
+
+}
---
+++ Assets/Scripts/UI/Attributes.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Attributes : MonoBehaviour
+{
+
+    public Text txt;
+    int ki;
+    int health;
+    string stringname;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        txt.text = "Attributes\n" +
+    "Strength:  " + CharacterManager.Strength + 
+    "\nWisdom: " + CharacterManager.Wisdom +
+    "\nDexterity: " + CharacterManager.Dexterity + 
+    "\nLuck: " + CharacterManager.Luck;
+    }
+}
